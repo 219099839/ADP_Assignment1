b2ef51bd4c7ab87eeb69291f3ff7bb738c0cd3d0
Titilayo Akinjole <220283982#mycput.ac.za>
2021-04-01 15:10:41 +0200

    Finished work for Titilayo Akinjole 220283982

---
+++ src/main/java/za/ac/cput/adp_assignment1/addNumber.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adp_assignment1;
+
+/**
+ *
+ * @author TITILAYO
+ */
+public class addNumber {
+                public int addMeth ( int val1, int val2){
+                    return val1 + val2 ;
+                }
+}
